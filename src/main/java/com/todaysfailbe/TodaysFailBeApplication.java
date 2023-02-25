16d4f8463e786afc194c33e427a4195d3ba9394d
kdomo <dongho5088@naver.com>
2023-02-25 15:54:05 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/todaysfailbe/TodaysFailBeApplication.java
@@ -0,0 +1,13 @@
+package com.todaysfailbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaysFailBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodaysFailBeApplication.class, args);
+	}
+
+}
