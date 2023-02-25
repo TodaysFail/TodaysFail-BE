fa4a9729dc2e87a745cfdac6bc5807b7b67a450a
kdomo <dongho5088@naver.com>
2023-02-26 02:16:24 +0900

    [#26] docs: ApiModelProperty를  ApiParam으로 변경

--- src/main/java/com/todaysfailbe/record/model/request/CreateRecordRequest.java
+++ src/main/java/com/todaysfailbe/record/model/request/CreateRecordRequest.java
@@ -4,7 +4,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,21 +19,21 @@ import lombok.ToString;
 @AllArgsConstructor
 public class CreateRecordRequest {
 	@NotBlank(message = "작성자을 필수입니다.")
-	@ApiModelProperty(notes = "작성자", required = true)
+	@ApiParam(value = "작성자", required = true)
 	private String writer;
 
 	@NotBlank(message = "제목은 필수입니다.")
 	@Size(max = 17, message = "제목은 17자 이하로 입력해주세요.")
-	@ApiModelProperty(notes = "제목", required = true)
+	@ApiParam(value = "제목", required = true)
 	private String title;
 
 	@NotBlank(message = "내용은 필수입니다.")
 	@Size(max = 300, message = "내용은 300자 이하로 입력해주세요.")
-	@ApiModelProperty(notes = "내용", required = true)
+	@ApiParam(value = "내용", required = true)
 	private String content;
 
 	@NotBlank(message = "느낀점은 필수입니다.")
 	@Size(max = 20, message = "느낀점은 20자 이하로 입력해주세요.")
-	@ApiModelProperty(notes = "느낀점", required = true)
+	@ApiParam(value = "느낀점", required = true)
 	private String feel;
 }
