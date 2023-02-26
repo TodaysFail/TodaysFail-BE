7d0f46fcc64391134add649a3641a213a7384cc8
kdomo <dongho5088@naver.com>
2023-02-26 20:34:51 +0900

    [#38] docs: Swagger 문서 수정 / 정리

--- src/main/java/com/todaysfailbe/record/model/request/CreateRecordRequest.java
+++ src/main/java/com/todaysfailbe/record/model/request/CreateRecordRequest.java
@@ -19,21 +19,21 @@ import lombok.ToString;
 @AllArgsConstructor
 public class CreateRecordRequest {
 	@NotBlank(message = "작성자을 필수입니다.")
-	@ApiModelProperty(value = "작성자", required = true)
+	@ApiModelProperty(value = "작성자", required = true, example = "도모")
 	private String writer;
 
 	@NotBlank(message = "제목은 필수입니다.")
 	@Size(max = 17, message = "제목은 17자 이하로 입력해주세요.")
-	@ApiModelProperty(value = "제목", required = true)
+	@ApiModelProperty(value = "제목", required = true, example = "핫케이크 태움")
 	private String title;
 
 	@NotBlank(message = "내용은 필수입니다.")
 	@Size(max = 300, message = "내용은 300자 이하로 입력해주세요.")
-	@ApiModelProperty(value = "내용", required = true)
+	@ApiModelProperty(value = "내용", required = true, example = "핫케이크를 태웠다. 끝.")
 	private String content;
 
 	@NotBlank(message = "느낀점은 필수입니다.")
 	@Size(max = 20, message = "느낀점은 20자 이하로 입력해주세요.")
-	@ApiModelProperty(value = "느낀점", required = true)
+	@ApiModelProperty(value = "느낀점", required = true, example = "다음에 더 잘하면 된다")
 	private String feel;
 }
