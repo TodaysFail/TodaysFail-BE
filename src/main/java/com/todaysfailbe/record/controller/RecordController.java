92dd177b29079ae78e730eba9785c6e015e79396
kdomo <dongho5088@naver.com>
2023-02-26 17:18:25 +0900

    [#28] feat: 실패 기록 조회 기능

--- src/main/java/com/todaysfailbe/record/controller/RecordController.java
+++ src/main/java/com/todaysfailbe/record/controller/RecordController.java
@@ -1,15 +1,22 @@
 package com.todaysfailbe.record.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.concurrent.ConcurrentMap;
+
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.todaysfailbe.record.domain.Record;
 import com.todaysfailbe.record.model.request.CreateRecordRequest;
+import com.todaysfailbe.record.model.request.RecordsRequest;
 import com.todaysfailbe.record.service.RecordService;
 
 import io.swagger.annotations.ApiOperation;
@@ -41,6 +48,28 @@ public class RecordController {
 	public ResponseEntity<Void> createRecord(@RequestBody @Valid CreateRecordRequest createRecordRequest) {
 		log.info("[RecordController.createRecord] 레코드 생성 요청");
 		recordService.createRecord(createRecordRequest);
+		log.info("[RecordController.createRecord] 레코드 생성 성공");
 		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+
+	@ApiOperation(
+			value = "실패 기록들을 조회합니다",
+			notes = "닉네임을 받아 해당 닉네임의 실패 기록들을 조회합니다"
+	)
+	@ApiResponses({
+			@ApiResponse(
+					code = 201, message = "API 정상 작동 / 실패 기록 목록 조회 성공"
+			),
+			@ApiResponse(
+					code = 400, message = "입력 값이 잘못되었을 경우입니다"
+			)
+	})
+	@GetMapping
+	public ResponseEntity<ConcurrentMap<LocalDate, List<Record>>> getRecords(
+			@RequestBody @Valid RecordsRequest recordsRequest) {
+		log.info("[RecordController.getRecords] 레코드 목록 조회 요청");
+		ConcurrentMap<LocalDate, List<Record>> response = recordService.getRecords(recordsRequest);
+		log.info("[RecordController.getRecords] 레코드 목록 조회 성공");
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 }
--- src/main/java/com/todaysfailbe/record/repository/RecordRepository.java
+++ src/main/java/com/todaysfailbe/record/repository/RecordRepository.java
@@ -1,8 +1,12 @@
 package com.todaysfailbe.record.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.todaysfailbe.member.domain.Member;
 import com.todaysfailbe.record.domain.Record;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
+	List<Record> findAllByMember(Member member);
 }
