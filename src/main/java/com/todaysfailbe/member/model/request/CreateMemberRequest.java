0389e5f978d2b28817e68c051c463df605f43748
kdomo <dongho5088@naver.com>
2023-02-26 20:25:47 +0900

    [#38] docs: Swagger 문서 수정 / 정리

--- src/main/java/com/todaysfailbe/member/model/request/CreateMemberRequest.java
+++ src/main/java/com/todaysfailbe/member/model/request/CreateMemberRequest.java
@@ -4,7 +4,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,6 +20,6 @@ import lombok.ToString;
 public class CreateMemberRequest {
 	@NotBlank(message = "이름은 필수입니다.")
 	@Size(max = 10, message = "이름은 10자 이하로 입력해주세요.")
-	@ApiParam(value = "닉네임", required = true)
+	@ApiModelProperty(value = "닉네임", required = true)
 	private String name;
 }
