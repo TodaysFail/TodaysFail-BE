7fc45d9ea33770fd1fc4b1319a6cc630d9595175
kdomo <dongho5088@naver.com>
2023-03-01 17:07:01 +0900

    [#65] refactor: EnableJpaAuditing config로 분리

--- src/main/java/com/todaysfailbe/TodaysFailBeApplication.java
+++ src/main/java/com/todaysfailbe/TodaysFailBeApplication.java
@@ -2,10 +2,8 @@ package com.todaysfailbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class TodaysFailBeApplication {
 
 	public static void main(String[] args) {
